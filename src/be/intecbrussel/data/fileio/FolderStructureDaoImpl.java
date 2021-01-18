17c4ef46b17bbc1aadc66b1ac15f24947521d1b5
Mert <mertdemirok80@gmail.com>
2021-01-18 10:04:42 +0100

    create copyFiles method

--- src/be/intecbrussel/data/fileio/FolderStructureDaoImpl.java
+++ src/be/intecbrussel/data/fileio/FolderStructureDaoImpl.java
@@ -3,9 +3,7 @@ package be.intecbrussel.data.fileio;
 import be.intecbrussel.data.FolderStructureDao;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.TreeSet;
 
 public class FolderStructureDaoImpl implements FolderStructureDao {
@@ -17,10 +15,24 @@ public class FolderStructureDaoImpl implements FolderStructureDao {
         Path pathToWalk = Paths.get("resources/unsorted/");
 
         try {
-            Files.walk(pathToWalk).forEach(path -> setDirectoryName(path));
             Files.createDirectory(Path.of("resources/sorted_folder"));
+
+            // set directory names
+            Files.walk(pathToWalk).forEach(path -> {
+                if(path.getFileName().toFile().isHidden()){
+                    System.out.println(path.getFileName());
+                }
+                setDirectoryName(path);
+            });
+
+            // create directories
             createDirectories();
-            copyFilesIntoDirectories();
+
+            // copy files into directories
+            Files.walk(pathToWalk).forEach(path -> {
+              copyFiles(path);
+            });
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -41,7 +53,10 @@ public class FolderStructureDaoImpl implements FolderStructureDao {
 
     private void setNonHiddenDirectoryNames(Path path) {
         folderNameSet.add("summary");
+        setExtensionsAsFolderNames(path);
+    }
 
+    private void setExtensionsAsFolderNames(Path path) {
         String fullName = path.getFileName()
                 .toString()
                 .replaceAll(" ", "");
@@ -67,9 +82,22 @@ public class FolderStructureDaoImpl implements FolderStructureDao {
         });
     }
 
-    private void copyFilesIntoDirectories(){
-
-    };
+    private void copyFiles(Path path) {
+        folderNameSet.forEach(name -> copyFileIntoDirectory(path, name));
+    }
+    private void copyFileIntoDirectory(Path path, String name){
+        if(path.getFileName().toString().contains(name)){
+            System.out.println("1: " + name);
+            Path p1 = Paths.get("/resources");
+            Path p2 = p1.resolve("sorted_folder");
+            Path copyDestination = p2.resolve(name);
+            try {
+                Files.copy(path, copyDestination, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public void summarizeFolder() {
